257746856d9366266e3b6c16ab8cf61a77dab3d1
SCHÄFER Eric <schafer490+github@gmail.com>
2022-12-24 18:17:43 +0100

    wip

--- calculus/main.py
+++ calculus/main.py
@@ -1,46 +1,16 @@
-import re
-from re import Match
-
-
 def decompose_calculus_line(line: str):
     match line.split(" "):
-        case [operand1, "+", operand2]:
+        case [left_operand, "+", right_operand]:
             print("add")
-        case [operand1, "-", operand2]:
+        case [left_operand, "-", right_operand]:
             print("sub")
-        case [operand1, "*", operand2]:
+        case [left_operand, "*", right_operand]:
             print("mul")
-        case [operand1, "/", operand2]:
+        case [left_operand, "/", right_operand]:
             print("div")
         case _:
             raise Exception(f"Not parseable line -- {line}")
 
 
-def decompose_operand(ope: str):
-    match ope.strip(")").split("("):
-        case ["Number", myvalue]:
-            print(f"NUMBER ==> {myvalue}")
-        case ["Time", myvalue]:
-            print(f"TIME ==> {myvalue}")
-        case [x, myvalue]:
-            raise Exception(f"Unknown operand type -- type={x}")
-
-
-def strtime_to_seconds(val: str):
-    re_pattern = re.compile(
-        """((?P<hours>\\d*)h)?((?P<minutes>\\d*)m)?((?P<seconds>\\d*)s)?"""
-    )
-    match re_pattern.search(val):
-        case None:
-            raise Exception(f"Time value not parseable -- value={val}")
-        case m:
-            return (
-                int(m.group("hours") if m.group("hours") else "0") * 3600
-                + int(m.group("minutes") if m.group("minutes") else "0") * 60
-                + int(m.group("seconds") if m.group("seconds") else "0")
-            )
-
-
 if __name__ == "__main__":
     print("Hello World")
-    print(strtime_to_seconds("1h"))
--- calculus/operand.py
+++ calculus/operand.py
@@ -1,23 +1,44 @@
 from abc import ABC
-from datetime import datetime
+import re
 
 
 class Operand(ABC):
-    def __init__(self) -> None:
-        self.value_as_int = 0
+    def __init__(self, myvalue: float):
+        self.value_as_number: float = myvalue
 
 
 class MyTime(Operand):
-    def __init__(self, time: str) -> None:
-        super().__init__()
+    def __init__(self, timestr: str):
+        super().__init__(self.strtime_to_seconds(timestr))
+
+    @staticmethod
+    def strtime_to_seconds(val: str) -> int:
+        re_pattern = re.compile(
+            """((?P<hours>\\d*)h)?((?P<minutes>\\d*)m)?((?P<seconds>\\d*)s)?"""
+        )
+        match re_pattern.search(val):
+            case None:
+                raise Exception(f"Time value not parseable -- value={val}")
+            case m:
+                return (
+                    int(m.group("hours") if m.group("hours") else "0") * 3600
+                    + int(m.group("minutes") if m.group("minutes") else "0") * 60
+                    + int(m.group("seconds") if m.group("seconds") else "0")
+                )
 
 
 class MyNumber(Operand):
     def __init__(self, num: str) -> None:
-        super().__init__()
+        super().__init__(float(num))
 
 
 class OperandFactory:
     @staticmethod
-    def build(datatype: str, datavalue: str):
-        pass
+    def build(ope: str) -> Operand:
+        match ope.strip(")").split("("):
+            case ["Number", myvalue]:
+                return MyNumber(myvalue)
+            case ["Time", myvalue]:
+                return MyTime(myvalue)
+            case _:
+                raise Exception(f"Unknown operand type -- type={ope}")
